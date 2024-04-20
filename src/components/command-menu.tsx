2f9330755a84c8360e1b0ecca0254c59ea7000c7
Joydip Roy <joydipand@gmail.com>
2024-04-20 14:08:30 +0530

    fixed: command menu

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -15,6 +15,7 @@ import { Button } from "./ui/button";
 import { CommandIcon } from "lucide-react";
 import { CommandMenuProps } from "@/types";
 import { ColorPalette } from "./color-palette";
+import { Label } from "./ui/label";
 
 export const CommandMenu = ({ links }: CommandMenuProps) => {
   const [open, setOpen] = React.useState(false);
@@ -33,21 +34,21 @@ export const CommandMenu = ({ links }: CommandMenuProps) => {
 
   return (
     <>
-      <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm xl:block print:hidden">
-        Press{" "}
-        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 text-[10px] font-medium opacity-100">
-          <span className="text-xs">⌘</span>J
-        </kbd>{" "}
-        to open the command menu
-      </p>
       <Button
         onClick={() => setOpen((open) => !open)}
         variant="outline"
         size="icon"
-        className="fixed bottom-4 right-4 flex rounded-full shadow-2xl xl:hidden print:hidden"
+        className="fixed bottom-4 right-4 flex rounded-full shadow-2xl print:hidden"
       >
         <CommandIcon className="h-4 w-4" />
       </Button>
+      <Label className="fixed bottom-0 left-0 right-0 p-1 text-center text-sm xl:block print:hidden">
+        Press{" "}
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded bg-muted border-1 px-1.5 text-[10px] font-medium opacity-100">
+          <span className="text-xs font-semibold">⌘ J</span>
+        </kbd>{" "}
+        to open the command menu
+      </Label>
       <CommandDialog open={open} onOpenChange={setOpen}>
         <CommandInput placeholder="Type a command or search..." />
         <CommandList>
