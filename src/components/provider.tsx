6135e55a2be1ca24a85fa59b1b2a41fedb224515
Joydip Roy <112172822+rjoydip@users.noreply.github.com>
2025-02-13 17:18:50 +0530

    eslint version upgrade to v9 (#30)
    * chore: bump eslint version to v9
    
    * chore: added missing package playwright

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import type { QueryClient } from '@tanstack/react-query'
+import type { ReactNode } from 'react'
 import { getQueryClient } from '@/lib/qclient'
 import { dehydrate, HydrationBoundary, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { type ReactNode, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { isDevelopment } from 'std-env'
 
 export default function Providers({ children }: { children: ReactNode }) {
